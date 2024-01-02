f3bc01b4537dec9031f1f819d07702c128bc0ef9
Maiara Ferreira <103464615+MaiaFerreira04@users.noreply.github.com>
2024-01-02 07:54:12 -0300

    feat: adição das tables

---
+++ lib/database/tables/lista_table.dart
@@ -0,0 +1,12 @@
+class TablePaciente {
+  static const tableName = 'frutas';
+
+  ///[INTEGER] - PK
+  static const id = 'id';
+
+  ///[TEXT]
+  static const title = 'title';
+
+  ///[TEXT]
+  static const iconPath = 'iconPath';
+}
